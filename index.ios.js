8acbc93952006f6cc81603c7a30c93e6c6e791c4
Andre Oriani <aoriani@walmartlabs.com>
2015-12-17 00:06:15 -0800

    Lecture 24

---
+++ index.ios.js
@@ -0,0 +1,68 @@
+var React = require('react-native');
+var {
+  Text,
+  View,
+  AppRegistry,
+  StyleSheet
+} = React;
+
+
+var StopWatch = React.createClass({
+  render: function() {
+      return <View style={styles.container}>
+
+        <View style={[styles.header, this.border('yellow')]}>
+          <View style={this.border('red')}>
+            <Text>00:00:00</Text>
+          </View>
+          <View style={this.border('green')}>
+            {this.startStopButton()}
+            {this.lapButton()}
+          </View>
+        </View>
+
+        <View style={[styles.footer, this.border('blue')]}>
+          <Text> List of Laps </Text>
+        </View>
+
+
+      </View> 
+  },
+
+  startStopButton: function() {
+    return <View>
+          <Text> Start </Text>
+        </View>
+  },
+
+  lapButton: function() {
+    return <View>
+          <Text> Lap </Text>
+        </View>
+  },
+
+  border: function(color) {
+    return {
+      borderColor: color,
+      borderWidth: 4
+    }
+  }
+
+});
+
+var styles = StyleSheet.create({
+  container : {
+    flex: 1,
+    alignItems: 'stretch'
+  },
+  
+  header: {
+    flex: 1
+  },
+
+  footer: {
+    flex: 1
+  }
+});
+
+AppRegistry.registerComponent('react_native_stopwatch', () => StopWatch);
