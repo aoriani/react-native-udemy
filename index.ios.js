cca01156901456477e6e131d7408c7bc20e6d059
Andre Oriani <aoriani@walmartlabs.com>
2015-12-21 00:32:07 -0800

    Lecture 31

--- index.ios.js
+++ index.ios.js
@@ -13,59 +13,64 @@ var StopWatch = React.createClass({
 
   getInitialState: function() {
     return {
-      timeElapsed: null
+      timeElapsed: null,
+      running : false
     }
   },
 
   render: function() {
       return <View style={styles.container}>
 
-        <View style={[styles.header, this.border('yellow')]}>
-          <View style={[styles.timeWrapper, this.border('red')]}>
-            <Text>{formatTime(this.state.timeElapsed)}</Text>
+        <View style={styles.header}>
+          <View style={styles.timeWrapper}>
+            <Text style={styles.timer}>{formatTime(this.state.timeElapsed)}</Text>
           </View>
-          <View style={[styles.buttonWrapper, this.border('green')]}>
+          <View style={styles.buttonWrapper}>
             {this.startStopButton()}
             {this.lapButton()}
           </View>
         </View>
 
-        <View style={[styles.footer, this.border('blue')]}>
+        <View style={styles.footer}>
           <Text> List of Laps </Text>
         </View>
-
 
       </View> 
   },
 
   startStopButton: function() {
+
+    var style = this.state.running ?  styles.stopButton : styles.startButton;
+
     return <TouchableHighlight 
               underlayColor="gray"
-              onPress={this.handleStartPress}>
-          <Text>Start</Text>
+              onPress={this.handleStartPress}
+              style={[styles.button, style]}>
+          <Text>{this.state.running ? "Stop" : "Start"}</Text>
         </TouchableHighlight>
   },
 
   lapButton: function() {
-    return <TouchableHighlight >
+    return <TouchableHighlight style={styles.button}>
           <Text>Lap</Text>
         </TouchableHighlight>
   },
 
-  border: function(color) {
-    return {
-      borderColor: color,
-      borderWidth: 4
-    }
-  },
-
   handleStartPress: function() {
+
+    if (this.state.running) {
+      clearInterval(this.interval)
+      this.setState({running: false});
+      return;
+    }
+
     var startTime = new Date();
 
-    setInterval(() =>
+    this.interval = setInterval(() =>
       {
         this.setState({
-          timeElapsed: new Date() - startTime
+          timeElapsed: new Date() - startTime,
+          running: true
         }); 
       }, 30);
     
@@ -98,7 +103,29 @@ var styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent:'space-around',
     alignItems: 'center'
+  },
+
+  timer: {
+    fontSize: 60
+  },
+
+  button: {
+    borderWidth: 2,
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    justifyContent: 'center', 
+    alignItems: 'center'
+  },
+
+  startButton : {
+    borderColor: 'green'
+  },
+
+  stopButton : {
+    borderColor: 'red'
   }
+
 });
 
 AppRegistry.registerComponent('react_native_stopwatch', () => StopWatch);
